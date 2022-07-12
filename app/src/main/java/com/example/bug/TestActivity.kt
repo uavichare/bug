11b5efb2ab511be500918f380be0c294f2c4376c
dilipgupta123-123 <GDIlip10#$%>
2022-07-12 09:56:04 +0530

    added dubai project

---
+++ app/src/main/java/com/example/bug/TestActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bug
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dubaiculture.smartguide.MainActivity
+import com.google.firebase.FirebaseApp
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+    }
+}
