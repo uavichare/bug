e0ab1e6366a6dac96ae5859cd57f603056f07e37
dilipgupta123-123 <GDIlip10#$%>
2022-07-21 13:37:14 +0530

    commented test activity code

--- app/src/main/java/com/example/bug/TestActivity.kt
+++ app/src/main/java/com/example/bug/TestActivity.kt
@@ -10,6 +10,10 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+       /* InsuideSDK.authenticationOfMapbox(this,"pk.eyJ1IjoibWVycmlsbC1hbW0iLCJhIjoiY2o4MnM0dm0wNTg5ODJ3bzNiajV1cHF2MyJ9.cLz-hmZiv5NbM4LGt-aBUA")
+        InsuideSDK.OpenSdkScreen(this)
+        InsuideSDK.authenticationInsuideIndoor(this,"675933d8-45d2-4397-aef6-80bcf5861fed","rxNJoW/xt1iVy3BA5c0r69tjf6097VxsW9dz56JzOnQsRbcD3qGDyKT0e3iA1XGEpn2N5JHL7FpgZSyuF5BKSXXWqJ+Y2nWqr8lXa5lmECBYOxiZzXnCih5Ozljgag==")
+*/
 
     }
 
